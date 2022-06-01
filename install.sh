8146680fb313bc6818e6a75f5470df91039cd666
therjyer <thgcastro07@gmail.com>
2022-06-01 16:55:03 -0300

    01/06/2022 - 16:55 - Version 0.0.1

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -y && pkg update -y
+sudo apt-get upgrade -y && pkg upgrade -y
+sudo apt-get install git -y && pkg install git -y
+sudo apt-get install imagemagick -y && pkg install imagemagick -y
+sudo apt-get install python3 -y && pkg install python3 -y
+sudo apt-get install pip -y && pkg install pip -y
+sudo apt-get install ffmpeg -y && pkg install ffmpeg -y
+sudo apt-get install nodejs -y && pkg install nodejs -y
+sudo apt-get install npm -y && pkg install npm -y
+sudo apt-get install tesseract -y && pkg install tesseract -y
+sudo apt-get install wget -y && pkg install wget -y
+sudo npm install -g ts-node -f && pkg install -g ts-node -f
+npm install && sudo npm install

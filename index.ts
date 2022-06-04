6339bdfceea73be147dc1ece1ae8f1ddf4f230b2
therjyer <thgcastro07@gmail.com>
2022-06-04 00:30:40 -0300

    04/06/2022 - 00:30 - Version 0.0.3 (QR programmed)

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
-const { Client, fromMe, LocalAuth } = require('whatsapp-web.js');
+// Conexão
+const { Client, fromMe, LocalAuth, NoAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+// Utilitários
+const fs = require('fs')
+
+// Módulos
+const setting = JSON.parse(fs.readFileSync('./data/json/settings.json'))
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: true }
+    //puppeteer: { headless: false }
 });
+  
+const prefix = setting.prefix
 
 client.initialize();
 
 client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
     console.log('Código recebido: ', qr);
 });
 
@@ -20,30 +31,25 @@ client.on('auth_failure', msg => {
 });
 
 client.on('ready', () => {
-    console.log('Pronto!');
+    console.log('Pronto para começar!');
 });
 
 client.on('message', async msg => {
     console.log('Mensagem recebida: ', msg);
 
-    // Teste de resposta
-        if (msg.body === '.ping') {
-            msg.reply('pong');
-
+        if (msg.body === `${prefix}ping`) {
+            (msg.reply('*pong*') || msg.reply('*pong2*'));
         };
 
-    // Teste de condição
-    if (msg.body === '!test') {
-        await msg.reply('teste');
+        if(msg.hasMedia === true && msg.body === `${prefix}fig`) {
+            const media = await msg.downloadMedia();
+            msg.reply(media);
+            // do something with the media data here
+        }
 
+        if (msg.body === `${prefix}exit`) {
+            msg.reply('_Desligando..._')
+            client.stop(); 
     };
 });
 
-client.on('call', async call => {
-    if (fromMe === true)
-        if (call.body === '!ping') {
-            call.reply('pong');
-
-        };
-
-});
